494d7090e8a69ba6c786bfa5570f3c814ff921b1
Milad Ahmadi <info@miladahmadi.net>
2022-05-25 10:47:37 +0430

    switch webpack pug loader to @webdiscus/pug-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,92 +5,95 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread'],
-          },
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      { loader: 'url-loader', test: /\.gif$/ },
-      { loader: 'file-loader', test: /\.(ttf|eot|svg)$/ },
-      {
-        test: /\.s[ac]ss$/i,
-        include: /\.component\.scss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[hash:base64:7]',
-                // namedExport: true,
-                exportOnlyLocals: false,
-              },
-              importLoaders: true,
-              esModule: true,
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
             },
-          },
-          // Compiles Sass to CSS
-          'sass-loader',
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread'],
+                    },
+                },
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {loader: 'url-loader', test: /\.gif$/},
+            {loader: 'file-loader', test: /\.(ttf|eot|svg)$/},
+            {
+                test: /\.s[ac]ss$/i,
+                include: /\.component\.scss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[hash:base64:7]',
+                                // namedExport: true,
+                                exportOnlyLocals: false,
+                            },
+                            importLoaders: true,
+                            esModule: true,
+                        },
+                    },
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.pug$/i,
+                loader: '@webdiscus/pug-loader',
+                options: {
+                    method: 'render',
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /\.component\.scss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    {
+                        loader: 'css-loader',
+                    },
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
         ],
-      },
-      {
-        test: /\.pug$/i,
-        loader: 'pug-loader',
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        exclude: /\.component\.scss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          {
-            loader: 'css-loader',
-          },
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
+    },
+    plugins: [
+        new Dotenv(),
+        new HtmlWebpackPlugin({
+            template: './src/views/index.pug',
+        }),
     ],
-  },
-  plugins: [
-    new Dotenv(),
-    new HtmlWebpackPlugin({
-      template: './src/views/index.pug',
-    }),
-  ],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    plugins: [new TsconfigPathsPlugin()],
-  },
-  output: {
-    filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 8000,
-  },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        plugins: [new TsconfigPathsPlugin()],
+    },
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8000,
+    },
 };
